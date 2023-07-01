ba738e76f480994771c241ca1641ef85638cece2
Jorge Daniel Cruz <90866381+JorgeDCruz@users.noreply.github.com>
2023-06-30 20:19:12 -0600

    Fixed issue while updating record

--- Node JS and Express/lesson 4/red-bicicletas/controllers/bicicleta.js
+++ Node JS and Express/lesson 4/red-bicicletas/controllers/bicicleta.js
@@ -30,7 +30,7 @@ exports.bicicleta_update_get = (req, res) => {
 
 exports.bicicleta_update_post = (req, res) => {
     let corresponding_bici = Bicicleta.findById(req.params.id);
-    //corresponding_bici.id = req.body.id;
+    corresponding_bici.id = req.body.id;
     corresponding_bici.color = req.body.color;
     corresponding_bici.modelo = req.body.modelo;
     corresponding_bici.ubicacion = [req.body.lat, req.body.lng];
