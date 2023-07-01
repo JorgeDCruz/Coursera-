98090010efa707d636cb57f5b56546e00738aa70
Jorge Daniel Cruz <90866381+JorgeDCruz@users.noreply.github.com>
2023-06-30 20:04:23 -0600

    Added MVC infraestructure

---
+++ Node JS and Express/lesson 4/red-bicicletas/controllers/bicicleta.js
@@ -0,0 +1,39 @@
+const Bicicleta = require('../models/bicicleta');
+
+//Renderizamos la vista con el listado de bicicletas actuales
+exports.bicicleta_list = (req, res) => {
+    res.render('bicicletas/index', {bicis: Bicicleta.allBicis})
+}
+
+//Hay 2 instancias de la vista, cuando se renderiza y cuando se añade la bicicleta
+exports.bicicleta_create_get = (req, res) => {
+    res.render('bicicletas/create');
+}
+
+exports.bicicleta_create_post = (req, res) => {
+    let bici = new Bicicleta(req.body.id, req.body.color, req.body.modelo);
+    bici.ubicacion = [req.body.lat, req.body.lng];
+    Bicicleta.add(bici);
+
+    res.redirect('/bicicletas')
+}
+
+exports.bicicleta_delete_post = (req, res) => {
+    Bicicleta.removeById(req.params.id);
+    res.redirect('/bicicletas');
+}
+
+exports.bicicleta_update_get = (req, res) => {
+    let corresponding_bici = Bicicleta.findById(req.params.id);
+    res.render('bicicletas/update', {bici: corresponding_bici});
+}
+
+exports.bicicleta_update_post = (req, res) => {
+    let corresponding_bici = Bicicleta.findById(req.params.id);
+    //corresponding_bici.id = req.body.id;
+    corresponding_bici.color = req.body.color;
+    corresponding_bici.modelo = req.body.modelo;
+    corresponding_bici.ubicacion = [req.body.lat, req.body.lng];
+
+    res.redirect('/bicicletas')
+}
---
+++ Node JS and Express/lesson 4/red-bicicletas/models/bicicleta.js
@@ -0,0 +1,46 @@
+const Bicicleta = function(id, color, modelo, ubicacion){
+    this.id = id;
+    this.color = color;
+    this.modelo = modelo;
+    this.ubicacion = ubicacion;
+}
+
+//Un prototipo permite añadir nuevos valores o atributos al constructor de un objeto
+//Podemos verlo como añadirle funciones a un constructor
+Bicicleta.prototype.toString = function(){
+    return "id: " + this.id + "\ncolor" + this.color;
+}
+
+
+Bicicleta.allBicis = [];
+
+Bicicleta.add = function(given_bicicleta){
+    Bicicleta.allBicis.push(given_bicicleta);
+}
+
+Bicicleta.findById = function(given_id){
+    const corresponding_bici = Bicicleta.allBicis.find(x => x.id == given_id);
+    if(corresponding_bici){
+        return corresponding_bici;
+    }  
+    else{
+        throw new Error(`ID: ${given_id} not found in collection`);
+    }
+}
+
+Bicicleta.removeById = function(given_id){
+    for(let i = 0; i < Bicicleta.allBicis.length; i++){
+        if(Bicicleta.allBicis[i].id === given_id){
+            Bicicleta.allBicis.splice(i, 1);
+            break;
+        }
+    }
+}
+
+const a = new Bicicleta(1, 'rojo', 'urbana', [20.736963, -103.435984]);
+const b = new Bicicleta(2, 'blanca', 'urbana', [20.734976, -103.435855]);
+
+Bicicleta.add(a);
+Bicicleta.add(b);
+
+module.exports = Bicicleta;
