f384bb5c58999a2754f9244e0da4a3490e4525ed
Jorge Daniel Cruz <90866381+JorgeDCruz@users.noreply.github.com>
2023-07-01 18:41:40 -0600

    Finished API implementation of Get, Post and Delete

--- Node JS and Express/lesson 4/red-bicicletas/app.js
+++ Node JS and Express/lesson 4/red-bicicletas/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const bicicletaRouter = require('./routes/bicicletas');
+const bicicletasAPIrouter = require('./routes/api/bicicletas');
 
 var app = express();
 
@@ -20,9 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bicicletas', bicicletaRouter);
+app.use('/api/bicicletas', bicicletasAPIrouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
