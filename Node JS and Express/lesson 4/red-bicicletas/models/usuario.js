994f948856249a77f3ea7d82489a27abfb39b6cd
Jorge Daniel Cruz <90866381+JorgeDCruz@users.noreply.github.com>
2023-07-06 17:18:00 -0600

    Finished tunning the user to support auth

--- Node JS and Express/lesson 4/red-bicicletas/models/usuario.js
+++ Node JS and Express/lesson 4/red-bicicletas/models/usuario.js
@@ -1,10 +1,51 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Reserva = require('./reserva');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
+const validateEmail = (email) => {
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(email);
+};
 
 const usuarioSchema = new Schema({
-    nombre: String
+    nombre: {
+        type: String,
+        trim: true,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'El email es obligatorio'],
+        lowercase: true,
+        validate: [validateEmail, 'Por favor ingrese un email valido'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+    },
+    password: {
+        type: String,
+        required: [true, 'La password es obligatoria'],
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+    verificado: {
+        type: Boolean,
+        default: false
+    }
 });
+
+usuarioSchema.pre('save', (next) => {
+    if(this.isModified('password')){
+        this.password = bcrypt.hashSync(this.password, saltRounds);
+    }
+    next();
+});
+
+usuarioSchema.methods.validatePassword = (given_password) => {
+    return bcrypt.compareSync(given_password, this.password);
+};
 
 usuarioSchema.methods.reservar = function(biciID, desde, hasta, cb){
     const reserva = new Reserva({usuario: this._id, bicicleta: biciID, desde: desde, hasta: hasta});
